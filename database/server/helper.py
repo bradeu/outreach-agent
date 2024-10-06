b65454fcdc0dca28b9c5e9d85a7658eb2c44eee9
“bradeu” <bradeu01@gmail.com>
2024-10-05 18:09:54 -0700

    finish db config

--- database/server/helper.py
+++ database/server/helper.py
@@ -13,33 +13,33 @@ pc = Pinecone(api_key=api_key)
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 class Helper:
-    def upsert_method(self, vector, index_name="test1", namespace="ns1"):
+    def upsert_method(self, vector_list, index_name="test1", namespace="ns1"):
         while not pc.describe_index(index_name).status['ready']:
             time.sleep(1)
 
         index = pc.Index(index_name)
 
         vectors = []
-        for v in vector:
-            vectors.append((str(uuid.uuid4()), v))
+        for v in vector_list:
+            vectors.append((str(uuid.uuid4()), v['embedding'], {'sentence' : v['sentence']}))
 
         return index.upsert(
             vectors=vectors,
             namespace=namespace
         )
 
-    def query_method(self, vector, top_k=10, index_name="test1", namespace="ns1"):
+    def query_method(self, vector_list, top_k=10, index_name="test1", namespace="ns1"):
         while not pc.describe_index(index_name).status['ready']:
             time.sleep(1)
 
         index = pc.Index(index_name)
-        
+
         result = []
         seen_ids = set()
-        for v in vector:
+        for v in vector_list:
             res = index.query(
                 namespace=namespace,
-                vector=v.tolist(),
+                vector=v['embedding'].tolist(),
                 top_k=top_k,
                 include_values=True,
                 include_metadata=True,
@@ -47,14 +47,17 @@ class Helper:
 
             for match in res['matches']:
                 if match['id'] not in seen_ids:
-                    result.append(match)
+                    result.append(match['metadata']['sentence'])
                     seen_ids.add(match['id'])
 
         return result
     
     def embed_sentences(self, sentences):
         embeddings = model.encode(sentences)
-        return embeddings
+        embed_list = []
+        for i in range(len(embeddings)):
+            embed_list.append({'embedding' : embeddings[i], 'sentence' : sentences[i]})
+        return embed_list
 
     def split_text_into_sentences(self, text):
         sentences = sent_tokenize(text)
--- database/server/main.py
+++ database/server/main.py
@@ -12,12 +12,14 @@ def root():
 def query_endpoint(text: str, top_k: int, index_name: str, namespace: str):
     sentences = helper_obj.split_text_into_sentences(text)
     vector = helper_obj.embed_sentences(sentences)
-    res = helper_obj.query_method(vector, top_k, index_name, namespace)
+    # res = helper_obj.query_method(vector, top_k, index_name, namespace)
+    res = helper_obj.query_method(vector)
     return res.to_dict()
 
 @app.post("/upsert/")
 def upsert_endpoint(text: str, index_name: str, namespace: str):
     sentences = helper_obj.split_text_into_sentences(text)
     vector = helper_obj.embed_sentences(sentences)
-    res = helper_obj.upsert_method(vector, index_name, namespace)
+    # res = helper_obj.upsert_method(vector, index_name, namespace)
+    res = helper_obj.upsert_method(vector)
     return res.to_dict()
