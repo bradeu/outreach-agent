5e04b665511dc0648204ce40d9ddff0a1cfd1aab
“bradeu” <bradeu01@gmail.com>
2024-10-19 15:16:00 -0700

    fix json request and add top k

--- database/server/main.py
+++ database/server/main.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from helper import helper_obj
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return await request.json()
 
 @app.get("/query/")
-def query_endpoint(text: str):
+async def query_endpoint(request: Request):
+    data = await request.json()
+    text = data.get("text")
+    
     sentences = helper_obj.split_text_into_sentences(text)
     vector = helper_obj.embed_sentences(sentences)
+    try:
+        top_k = int(data.get("top_k"))
+        res = helper_obj.query_method(vector, top_k)
+    except:
+        res = helper_obj.query_method(vector)
     # res = helper_obj.query_method(vector, top_k, index_name, namespace)
-    res = helper_obj.query_method(vector)
     return res
 
 @app.post("/upsert/")
-def upsert_endpoint(text: str):
+async def upsert_endpoint(request: Request):
+    data = await request.json()
+    text = data.get("text")
+
     sentences = helper_obj.split_text_into_sentences(text)
     vector = helper_obj.embed_sentences(sentences)
-    # res = helper_obj.upsert_method(vector, index_name, namespace)
     res = helper_obj.upsert_method(vector)
+    # res = helper_obj.upsert_method(vector, index_name, namespace)
     return res
